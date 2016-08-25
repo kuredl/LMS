27b68e5952055b7ced37699eb2f4f1c8e451e74c
kuredl <kurt.edlund@it-pilot.com>
2016-08-25 15:26:45 +0200

    Ny klass Course

---
+++ LMS_Projekt/LMS.Server/Models/Course.cs
@@ -0,0 +1,21 @@
+﻿using LMS_Server.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LMS.Server.Models
+{
+    public class Course
+    {
+
+        [Key]
+        public int CourseId { get; set; }
+        public string CourseSubject { get; set; }
+
+        public virtual ICollection<Lesson> Lessons { get; set; }
+        public virtual ICollection<ApplicationUser> Attendants { get; set; }
+    }
+}
