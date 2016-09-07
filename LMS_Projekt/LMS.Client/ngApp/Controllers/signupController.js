a9891cb33824eb5f6ad5bd1b46f1f63e6d7901c3
kuredl <kurt.edlund@it-pilot.com>
2016-09-07 08:51:07 +0200

    new user management

--- LMS_Projekt/LMS.Client/ngApp/Controllers/signupController.js
+++ LMS_Projekt/LMS.Client/ngApp/Controllers/signupController.js
@@ -1,12 +1,14 @@
 ﻿'use strict';
-app.controller('signupController', ['$scope', '$location', '$timeout', 'authService', function ($scope, $location, $timeout, authService) {
-
+app.controller('signupController', ['$scope', '$http', 'API', '$location', '$timeout', 'authService', function ($scope, $http, API, $location, $timeout, authService) {
+    $scope.pageClass = 'page-home';
     $scope.savedSuccessfully = false;
     $scope.message = "";
-
+    $scope._Users;
+    $scope._Roles;
+    
     $scope.registration = {
         userName: "",
-        password: "",
+        passWord: "",
         confirmPassword: ""
     };
 
@@ -36,5 +38,44 @@ app.controller('signupController', ['$scope', '$location', '$timeout', 'authServ
             $location.path('/login');
         }, 2000);
     }
+
+    $scope.getUsers = function () {
+        API.get('api/admin/GetAllUsers').then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                data[i].selected = false;
+            }
+            $scope._Users = data;
+        });
+    };
+
+    $scope.getRoles = function () {
+        API.get('api/admin/GetRoles').then(function (data) {
+            console.log(data);
+            for (var i = 0; i < data.length; i++) {
+                data[i].selected = false;
+            }
+            $scope._Roles = data;
+        });
+    };
+
+    $scope.addRoles = function () {
+        var roles = $scope._Roles;
+        for (var i = 0; i < roles.length; i++) {
+            console.log("looping");
+            if (roles[i].selectedRole)
+                addUserToRole(roles[i].id, $scope.selectedUser);
+        }
+        $scope.getRoles();
+
+    };
+
+    var addUserToRole = function (r1, u2) {
+        console.log("hej");
+        API.get('api/admin/AddUserToRole/?rId=' + r1 + '&uId=' + u2).then(function (data) {
+            console.log(data);
+            $scope._Roles = data;
+        });
+    };
 
 }]);
